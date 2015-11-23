2cc2de0225ec1687937b9326ab0fac70ec4ddaea
Chennakesava <manikanta019@gmail.com>
2015-11-23 12:14:31 -0500

    Adding changes to kafka

--- kafkaComponents/src/main/java/com/chennakesava/kafka/ProductsProducer.java
+++ kafkaComponents/src/main/java/com/chennakesava/kafka/ProductsProducer.java
@@ -3,37 +3,60 @@ package com.chennakesava.kafka;
 import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
-
+import org.json.JSONObject;
+import org.json.JSONException;
 import kafka.producer.KeyedMessage;
-import kafka.producer.Producer;
+import kafka.javaapi.producer.Producer;
 import kafka.producer.ProducerConfig;
-import scala.collection.Seq;
 
 public class ProductsProducer {
 	public static void main(String[] args) {
 		long events = Long.parseLong(args[0]);
 		Random rnd = new Random();
-		
-		Properties props = new Properties();
-		props.put("metadata.broker.list", "broker1:9092");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("partitioner.class", "example.producer.SimplePartitioner");
-        props.put("request.required.acks", "1");
- 
-        ProducerConfig config = new ProducerConfig(props);
- 
-        Producer<String, String> producer = new Producer<String, String>(config);
- 
-        for (long nEvents = 0; nEvents < events; nEvents++) { 
-               long runtime = new Date().getTime();  
-               String productName = "sampleProduct_name" + rnd.nextInt(255);
-               String price = String.valueOf(rnd.nextInt(1000));
-               String msg = runtime +","+productName+","  + price; 
-               KeyedMessage<String, String> data = new KeyedMessage<String, String>("products",msg);
-               producer.send((Seq<KeyedMessage<String, String>>) data);
-        }
-        producer.close();
+		try {
+			Properties props = new Properties();
+			//set broker address
+			props.put("metadata.broker.list", "104.196.22.222:9092");
+	        props.put("serializer.class", "kafka.serializer.StringEncoder");
+	        //partitioner class is set default to serializer class
+	        //props.put("partitioner.class", "example.producer.SimplePartitioner");
+	        props.put("request.required.acks", "1");
+	 
+	        ProducerConfig config = new ProducerConfig(props);
+	 
+	        Producer<String, String> producer = new Producer<String, String>(config);
+	        //read a file or csv file here and send parsed text to kafka
+	        for (long nEvents = 0; nEvents < events; nEvents++) { 
+	               long runtime = new Date().getTime();  
+	               String productName = "sampleProduct_name_" + rnd.nextInt(255);
+	               String price = String.valueOf(rnd.nextInt(1000));
+	               String topic = "Amazon";
+	               String msg = runtime +","+productName+ "," + price; 
+	               System.out.println("Sending msg: "+msg);
+	               KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic,convertLineToJson(msg));
+	               producer.send(data);
+	        }
+	        
+	        System.out.println("Sent events:"+events);
+	        producer.close();
+		} catch (Exception e) {
+			System.err.println("Caught exception : "+e.getMessage());
+		}
 				
+	}
+	
+	public static String convertLineToJson(final String line) {
+		try {
+			String[] arr = line.split(",");
+			JSONObject record = new JSONObject();
+			record.put("time_stamp",arr[0]);
+			record.put("product_name", arr[1]);
+			record.put("price",arr[2]);
+			return record.toString();
+		} catch(Exception e) {
+			System.err.println("Caught exception: "+e.getMessage());
+			return null;
+		}
 	}
 
 }
